5bd2877b50796a4a84aba03d3dcf9f35ff08628b
Partheeban Janarthanan <partheeban.janarthanan@kp.org>
2017-08-31 11:16:49 +0530

    NEWS : Removed URL textview and made list item clikcable.

--- app/src/main/java/org/news/partheebanj/news/adapter/ArticleListAdapter.java
+++ app/src/main/java/org/news/partheebanj/news/adapter/ArticleListAdapter.java
@@ -1,6 +1,8 @@
 package org.news.partheebanj.news.adapter;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +18,8 @@ import org.news.partheebanj.news.model.Article;
 import org.news.partheebanj.news.util.StringUtil;
 
 import java.util.List;
+
+import static org.news.partheebanj.news.R.id.description_tv;
 
 /**
  * Created by partheebanj on 8/31/17.
@@ -35,17 +39,28 @@ public class ArticleListAdapter extends ArrayAdapter<Article> {
 
     @NonNull
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = context.getLayoutInflater();
         View rowView = inflater.inflate(R.layout.article_list_row, null, true);
-
 
         TextView header_tv = (TextView) rowView.findViewById(R.id.article_header_tv);
         TextView author_tv = (TextView) rowView.findViewById(R.id.article_author_tv);
         TextView description_tv = (TextView) rowView.findViewById(R.id.description_tv);
-        TextView url_tv = (TextView) rowView.findViewById(R.id.url_tv);
         TextView published_tv = (TextView) rowView.findViewById(R.id.published_tv);
         ImageView article_iv = (ImageView) rowView.findViewById(R.id.article_iv);
+
+
+        rowView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!StringUtil.isEmptyString(articleList.get(position).getURL())) {
+                    Intent i = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse(articleList.get(position).getURL()));
+                    context.startActivity(i);
+                }
+            }
+        });
+
 
         if (!StringUtil.isEmptyString(articleList.get(position).getTitle())) {
             header_tv.setVisibility(View.VISIBLE);
@@ -59,11 +74,6 @@ public class ArticleListAdapter extends ArrayAdapter<Article> {
         if (!StringUtil.isEmptyString(articleList.get(position).getDescription())) {
             description_tv.setVisibility(View.VISIBLE);
             description_tv.setText(articleList.get(position).getDescription());
-        }
-
-        if (!StringUtil.isEmptyString(articleList.get(position).getURL())) {
-            url_tv.setVisibility(View.VISIBLE);
-            url_tv.setText(articleList.get(position).getURL());
         }
 
         if (!StringUtil.isEmptyString(articleList.get(position).getPublishedAt())) {
